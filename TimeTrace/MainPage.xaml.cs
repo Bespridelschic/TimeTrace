cdeca48b3a82ff76fdc3f7a2fb63b0136d6cea05
Артём <bespridelschic@gmail.com>
2017-11-05 18:32:33 +1000

    Commit 05.11.2017 18:30

--- TimeTrace/MainPage.xaml.cs
+++ TimeTrace/MainPage.xaml.cs
@@ -17,6 +17,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+using TimeTrace.Model;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Windows.Storage;
+using System.Security.Cryptography;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -38,6 +45,23 @@ namespace TimeTrace
 			SolidColorBrush brush = new SolidColorBrush(color);
 			LoginButton.BorderBrush = brush;
 			HeaderText.Foreground = brush;
+
+			if (Directory.Exists(@"D:\Downloads\file.dat"))
+			{
+				using (StreamReader sr = File.OpenText(@"D:\Downloads\file.dat"))
+				{
+					LoginTextBox.Text = sr.ReadLine();
+				}
+			}
+
+			// Лямбда обработки клавиши Enter на главном экране
+			KeyUp += (sender, e) =>
+				{
+					if (e.Key == Windows.System.VirtualKey.Enter)
+					{
+						LoginInSystemButton(this, e);
+					}
+				};
 		}
 
 		/// <summary>
@@ -45,29 +69,92 @@ namespace TimeTrace
 		/// </summary>
 		private async void LoginInSystemButton(object sender, RoutedEventArgs e)
 		{
-			try
+			User user = new User(LoginTextBox.Text, PasswordTextBox.Password);
+
+			if (LoginTextBox.Text == string.Empty || PasswordTextBox.Password == string.Empty)
 			{
-				WebRequest request = WebRequest.Create("http://o129pak8.beget.tech/site/hello");
-				WebResponse response = await request.GetResponseAsync();
+				await (new MessageDialog("Одно из полей не задано", "Ошибка входа")).ShowAsync();
 
-				Stream webStream = response.GetResponseStream();
-				string responseMessage = string.Empty;
-
-				using (StreamReader sr = new StreamReader(webStream))
+				if (LoginTextBox.Text == string.Empty)
 				{
-					string line = "";
-					while ((line = sr.ReadLine()) != null)
-					{
-						responseMessage += line;
-					}
+					LoginTextBox.Focus(FocusState.Keyboard);
 				}
+				else
+				{
+					PasswordTextBox.Focus(FocusState.Keyboard);
+				}
+				return;
+			}
 
-				await (new MessageDialog($"{responseMessage}", "Результат запроса")).ShowAsync();
-			}
-			catch (Exception ex)
+			if (user.Email == "BadEmail")
 			{
-				await (new MessageDialog($"{ex.Message}", "Ошибка запроса!")).ShowAsync();
+				await (new MessageDialog("Не правильно введен Email", "Ошибка входа")).ShowAsync();
+				return;
 			}
+
+			//await (new MessageDialog($"{user.PasswordEncrypt()}", "Зашифрованный пароль")).ShowAsync(); return;
+
+			if (SavePassword.IsChecked == true)
+			{
+				StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+				StorageFile storageFile = await storageFolder.CreateFileAsync("PSF.bin", CreationCollisionOption.ReplaceExisting);
+
+				await FileIO.WriteTextAsync(storageFile, PasswordTextBox.Password);
+			}
+
+			//return;
+
+			//try
+			//{
+			// http://o129pak8.beget.tech/site/hello
+
+			/*WebRequest request = WebRequest.Create("http://o129pak8.beget.tech/site/signup");
+			request.Method = "POST"; // для отправки используется метод Post
+									 // данные для отправки
+			string data = user.JsonSerialize();
+			// преобразуем данные в массив байтов
+			byte[] byteArray = Encoding.UTF8.GetBytes(data);
+			// устанавливаем тип содержимого - параметр ContentType
+			request.ContentType = "application/x-www-form-urlencoded";
+			// Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+			//request.ContentLength = byteArray.Length;
+
+			//записываем данные в поток запроса
+			using (Stream dataStream = request.GetRequestStreamAsync())
+			{
+				dataStream.Write(byteArray, 0, byteArray.Length);
+			}
+
+			WebResponse response = await request.GetResponseAsync();
+			using (Stream stream = response.GetResponseStream())
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					Console.WriteLine(reader.ReadToEnd());
+				}
+			}*/
+			//response.Close();
+
+			//	WebRequest request = WebRequest.Create("http://o129pak8.beget.tech/site/signup");
+			//	WebResponse response = await request.GetResponseAsync();
+
+			//	Stream webStream = response.GetResponseStream();
+			//	string responseMessage = string.Empty;
+
+			//	using (StreamReader sr = new StreamReader(webStream))
+			//	{
+			//		string line = "";
+			//		while ((line = sr.ReadLine()) != null)
+			//		{
+			//			responseMessage += line;
+			//		}
+			//	}
+
+			//	await (new MessageDialog($"{responseMessage}", "Результат запроса")).ShowAsync();
+			//}
+			//catch (Exception ex)
+			//{
+			//	await (new MessageDialog($"{ex.Message}", "Ошибка запроса!")).ShowAsync();
 		}
 	}
 }
---
+++ TimeTrace/Model/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TimeTrace.Model
+{
+	public class User
+	{
+		#region Свойства
+		private string email;
+		public string Email
+		{
+			get { return email; }
+			set
+			{
+				string pattern = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+				var res = Regex.Match(value, pattern);
+
+				if (res.Success)
+				{
+					email = value;
+				}
+				else
+				{
+					email = "BadEmail";
+				}
+			}
+		}
+
+		private string password;
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+		#endregion
+
+		#region Конструкторы
+		public User()
+		{
+
+		}
+
+		public User(string email, string password)
+		{
+			Email = email;
+			Password = password;
+		}
+		#endregion
+
+		/// <summary>
+		/// Сериализация объекта User в формат Json
+		/// </summary>
+		/// <returns>Строка в формате Json</returns>
+		public string JsonSerialize()
+		{
+			var res = new { email = email, password = password };
+			return JsonConvert.SerializeObject(res);
+		}
+
+		/// <summary>
+		/// Десериализация объекта User в формате Json
+		/// </summary>
+		/// <param name="jsonString">Строка в формате Json</param>
+		/// <returns></returns>
+		public User JsonDeserialize(string jsonString)
+		{
+			User user = JsonConvert.DeserializeObject<User>(jsonString);
+			return user;
+		}
+
+		public string PasswordEncrypt()
+		{
+			SHA512 hash = SHA512.Create();
+
+
+			TripleDES tripleDES = TripleDES.Create();
+			tripleDES.KeySize = 128;
+			tripleDES.BlockSize = 128;
+			tripleDES.Padding = PaddingMode.PKCS7;
+			tripleDES.Mode = CipherMode.CBC;
+			tripleDES.Key = Encoding.UTF8.GetBytes("TimeTraceSecretCodeForPassword");
+
+			ICryptoTransform cryptoTransform = tripleDES.CreateEncryptor();
+			byte[] passwordIdByte = Encoding.UTF8.GetBytes(password);
+			byte[] cryptoPassword = cryptoTransform.TransformFinalBlock(passwordIdByte, 0, passwordIdByte.Length);
+
+			return Convert.ToBase64String(cryptoPassword);
+		}
+
+		#region Overrides
+		public override string ToString()
+		{
+			return $"{Email}, {Password}";
+		}
+		#endregion
+	}
+}
