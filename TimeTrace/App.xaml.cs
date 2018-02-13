ca877af7d60cb60da447d794335f56e4d439f9bc
Артём <Bespridelschic@gmail.com>
2018-02-14 00:45:41 +1000

    Changed binding for maps

--- TimeTrace/App.xaml.cs
+++ TimeTrace/App.xaml.cs
@@ -82,8 +82,8 @@ namespace TimeTrace
 					// Если стек навигации не восстанавливается для перехода к первой странице,
 					// настройка новой страницы путем передачи необходимой информации в качестве параметра
 					// параметр
-					rootFrame.Navigate(typeof(View.MainView.StartPage), e.Arguments);
-					//rootFrame.Navigate(typeof(View.AuthenticationView.SignInPage), e.Arguments);
+					//rootFrame.Navigate(typeof(View.MainView.StartPage), e.Arguments);
+					rootFrame.Navigate(typeof(View.AuthenticationView.SignInPage), e.Arguments);
 				}
 				// Обеспечение активности текущего окна
 				Window.Current.Activate();
